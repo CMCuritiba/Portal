c1c8d91b3936a77de3d92cca1a51d322c69c8f7c
Vinicius <viniciustom@hotmail.com>
2025-05-21 10:27:34 -0300

    merge in editoria

---
+++ backend/src/camara_de_curitiba/src/camara_de_curitiba/serializers/parent.py
@@ -0,0 +1,46 @@
+from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.serializer.converters import json_compatible
+from plone.restapi.serializer.dxcontent import SerializeToJson
+from plone.api import content
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import Interface
+from camara_de_curitiba.content.behaviors import IEditoriaBehavior
+from camara_de_curitiba.content.editoria import IEditoria
+
+
+@implementer(ISerializeToJson)
+@adapter(Interface)
+class ParentSerializer(SerializeToJson):
+    def __call__(self, *args, **kwargs):
+        result = super(ParentSerializer, self).__call__(*args, **kwargs)
+        
+        # Obtém o parent do contexto
+        parent = self.context.__parent__
+        
+        # Verifica se o parent é uma editoria usando a interface
+        if parent and IEditoria.providedBy(parent):
+            # Adiciona a cor de fundo ao resultado
+            result["cor_fundo"] = json_compatible(parent.cor_fundo)
+            result["editoria"] = {
+                "@id": parent.absolute_url(),
+                "title": parent.title,
+                "description": parent.description,
+                "cor_fundo": json_compatible(parent.cor_fundo)
+            }
+        else:
+            # Verifica se o conteúdo tem o behavior de editoria
+            editoria_behavior = IEditoriaBehavior(self.context, None)
+            if editoria_behavior is not None:
+                editoria_uid = editoria_behavior.editoria
+                if editoria_uid:
+                    editoria = content.get(UID=editoria_uid)
+                    if editoria:
+                        result["editoria"] = {
+                            "@id": editoria.absolute_url(),
+                            "title": editoria.title,
+                            "description": editoria.description,
+                            "cor_fundo": json_compatible(editoria.cor_fundo)
+                        }
+            
+        return result 
