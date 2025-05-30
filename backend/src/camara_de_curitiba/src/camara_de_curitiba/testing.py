2ffaf155d6e6314b8759e149bf379127cd080593
Vinicius <viniciustom@hotmail.com>
2025-05-30 16:45:37 -0300

    Image file

--- backend/src/camara_de_curitiba/src/camara_de_curitiba/testing.py
+++ backend/src/camara_de_curitiba/src/camara_de_curitiba/testing.py
@@ -5,9 +5,6 @@ from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
 from plone.testing.zope import WSGI_SERVER_FIXTURE
-from plone.app.testing import quickInstallProduct
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
 
 import camara_de_curitiba
 
@@ -21,27 +18,13 @@ class Layer(PloneSandboxLayer):
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
         import plone.restapi
-        import pas.plugins.oidc
-        import pas.plugins.keycloakgroups
 
         self.loadZCML(package=plone.restapi)
-        self.loadZCML(package=pas.plugins.oidc)
-        self.loadZCML(package=pas.plugins.keycloakgroups)
         self.loadZCML(package=camara_de_curitiba)
 
     def setUpPloneSite(self, portal):
-        # Instala os produtos necessários
-        quickInstallProduct(portal, "pas.plugins.oidc")
-        quickInstallProduct(portal, "pas.plugins.keycloakgroups")
-        
-        # Aplica os perfis
-        applyProfile(portal, "pas.plugins.oidc:default")
-        applyProfile(portal, "pas.plugins.keycloakgroups:default")
         applyProfile(portal, "camara_de_curitiba:default")
         applyProfile(portal, "camara_de_curitiba:initial")
-        
-        # Configura as permissões necessárias
-        setRoles(portal, TEST_USER_ID, ["Manager"])
 
 
 FIXTURE = Layer()
