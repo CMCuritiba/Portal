67b46d85c31108edf3ccd3251fd674643404adde
Vinicius <viniciustom@hotmail.com>
2025-05-30 09:44:13 -0300

    Image file

--- backend/src/camara_de_curitiba/src/camara_de_curitiba/setuphandlers/__init__.py
+++ backend/src/camara_de_curitiba/src/camara_de_curitiba/setuphandlers/__init__.py
@@ -4,6 +4,7 @@ from camara_de_curitiba.setuphandlers import users
 from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+import os
 
 
 @implementer(INonInstallable)
@@ -18,6 +19,11 @@ class HiddenProfiles(object):
 def populate_portal(context):
     """Post install script"""
     portal = api.portal.get()
+    # Ativa os complementos OIDC e Keycloak antes de popular as credenciais
+    qi = api.portal.get_tool('portal_quickinstaller')
+    for product in ["pas.plugins.oidc", "collective.keycloak"]:
+        if not qi.isProductInstalled(product):
+            qi.installProduct(product)
     # Delete content
     content.delete_content(portal)
     logger.info("Deleted default portal content")
@@ -32,3 +38,19 @@ def populate_portal(context):
     logger.info("Created initial content")
     # Update cover content
     content.update_home(portal, creators)
+    update_keycloak(portal)
+
+
+def update_keycloak(portal):
+    """Set keycloak information on acl_users."""
+    plugin = portal.acl_users.oidc
+    payload = {
+        "issuer": os.environ.get("KEYCLOAK_ISSUER", "http://sso.localhost/realms/plone"),
+        "client_id": os.environ.get("KEYCLOAK_CLIENT_ID", "plone"),
+        "client_secret": os.environ.get("KEYCLOAK_CLIENT_SECRET", "12345678"),  # nosec B105
+        "scope": os.environ.get("KEYCLOAK_OPEN_ID_SCOPES", "openid,profile,email").split(","),
+        "create_restapi_ticket": True,
+        "redirect_uris": [os.environ.get("KEYCLOAK_REDIRECT_URI", "http://localhost:3000/login-oidc/oidc")],
+    }
+    for key, value in payload.items():
+        setattr(plugin, key, value)
